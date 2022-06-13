5f81223a7e74f4a97c935007c2f61cb460eba338
Katty57 <kate.tyan57@gmail.com>
2022-06-13 12:54:02 +0600

    checking price array period deleted

--- Stocks/Screens/DetailsScreen/Model/DetailsModel.swift
+++ Stocks/Screens/DetailsScreen/Model/DetailsModel.swift
@@ -18,9 +18,9 @@ struct DetailsModel {
     static func build(from charts: Details) -> DetailsModel {
         let prices = charts.prices.map { $0.price }
         
-        return DetailsModel(periods: [Period(name: "W", prices: (prices.count >= 7 ? prices.suffix(7) : []) ),
-                                     Period(name: "M", prices: (prices.count >= 30 ? prices.suffix(30) : []) ),
-                                     Period(name: "6M", prices: (prices.count >= 180 ? prices.suffix(180) : []) ),
-                                     Period(name: "1Y", prices: (prices.count >= 365 ? prices.suffix(365) : []) )])
+        return DetailsModel(periods: [Period(name: "W", prices: prices.suffix(7)),
+                                     Period(name: "M", prices: prices.suffix(30)),
+                                     Period(name: "6M", prices: prices.suffix(180)),
+                                     Period(name: "1Y", prices: prices.suffix(365))])
     }
 }
